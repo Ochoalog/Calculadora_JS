e6ce17ce2b54d0cbdedd3be9b98f7d407319f443
Vitor Ochoa <vitorochoa12@gmail.com>
2021-04-26 23:50:13 -0300

    Calculadora usando Function Factory

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+/*
 function criaCalculadora() {
     return {
     display: document.querySelector('.display'),
@@ -8,7 +9,7 @@ function criaCalculadora() {
 
     pressionaEnter(){
         this.display.addEventListener('keyup', (e) => {
-            if(e.keyCode == 13){
+            if(e.KeyCode == 13){
                 this.calcula();
             }
         });
@@ -68,4 +69,57 @@ function criaCalculadora() {
 }
 
 const calculadora = criaCalculadora();
+calculadora.inicia();
+*/
+
+function Calculadora() {
+    this.display = document.querySelector('.display');
+    this.inicia = () => {
+        this.capturaClicks();
+        this.capturaEnter();
+    }; 
+
+    this.capturaEnter = () => {
+        this.display.addEventListener('keyup', e => {
+            if(e.keyCode == 13){
+                this.calcula();
+            }
+        });
+    };
+
+    this.capturaClicks = () => {
+        document.addEventListener('click', e => {
+            const el = e.target;
+            if(el.classList.contains('btn-num')) this.addNumdisplay(el);
+            if(el.classList.contains('btn-eq')) this.calcula();
+            if(el.classList.contains('btn-clear')) this.clear();
+            if(el.classList.contains('btn-del')) this.del();
+        });
+    };
+
+    this.calcula = () => {
+        try {
+            let conta = eval(this.display.value);
+
+            if(!conta){
+                alert('Conta invalida');
+                return;
+            }
+
+            this.display.value = String(conta);
+        } 
+        catch {
+            alert('Conta invalida');
+            return;
+        }
+    };
+
+    this.addNumdisplay = el => this.display.value += el.innerText;    
+    this.clear = () => this.display.value = '';
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
+    
+            
+}
+
+const calculadora = new Calculadora();
 calculadora.inicia();
